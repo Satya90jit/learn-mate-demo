2cfa09f4f79f4b85502d70744800b1762d36b595
Satya90jit <satyajitsahu2002@gmail.com>
2024-03-29 21:22:12 +0530

    Small Change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,20 +5,12 @@ import HeroSection from "@/components/home/HeroSection";
 const Home = () => {
   return (
     <>
-<<<<<<< HEAD
       <section className="bg-white">
         <Navbar />
         <HeroSection />
-        <CardSection />
+        {/* <CardSection /> */}
         <Footer />
       </section>
-=======
-      <Navbar />
-      <HeroSection />
-      <HeroSection />
-      <HeroSection />
-      <Footer />
->>>>>>> 5623578 (Changes made)
     </>
   );
 };
